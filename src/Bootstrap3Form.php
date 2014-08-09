44f19e76b10941d9185eadf4cd1fad4d8fa686a9
Philippe Gerber <philippe@bigwhoop.ch>
2014-08-09 13:18:37 +0200

    fixed namespace

--- src/Bootstrap3Form.php
+++ src/Bootstrap3Form.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace bigwhoop\Formular\Form;
+namespace bigwhoop\Formular\Form\Bootstrap3;
 use bigwhoop\Formular\Form;
 use bigwhoop\Formular\TemplateFactory\FileBasedFactory;
 
